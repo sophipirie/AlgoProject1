e62d574fd3dc3ad7cfb080ead8f93f2619a72d61
katrinale456 <113145732+katrinale456@users.noreply.github.com>
2023-02-06 16:42:15 -0500

    Add files via upload

--- LockBox.cpp
+++ LockBox.cpp
@@ -2,8 +2,24 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include "LockBox.h"
 using namespace std;
+
+class LockBox {
+	private:
+		int length;
+		int range;
+		vector <int> randomCode;
+		vector <int> guess;
+	public:
+		LockBox(int, int);
+		void setRandomCode(string);
+		void generateCode(int, int);
+		vector<int> getCode();
+		void setGuess(string s);
+		vector<int> getGuess();
+		int correctLocation(LockBox *);
+		int incorrectLocation(LockBox *);
+};
 
 LockBox::LockBox(int n, int m) {
 	length = n;
@@ -33,7 +49,7 @@ void LockBox::setGuess(string s) {
 	for (int i=0; i < s.size(); i++ ) {
                 if (isdigit(s[i])) {
                         v.push_back(s[i] - '0');
-                }
+		} 
         }
         guess = v;
 }
@@ -59,36 +75,17 @@ int LockBox::incorrectLocation(LockBox *g) {
 	vector<int> x = this->getCode();
 	vector<int> y = g->getGuess();
 	int incorrect = 0;
-	vector<int> copyCode(x.size());
-	vector<int> x1;
-	vector<int> y1;
-/*
-	for (int i = 0; i < x.size(); i++) {
-		for (int j = 0; j < x.size(); i++) {
-			if (x.at(i) == y.at(i) && i==j) {
-				copyCode[i] = 1;
-			}
-			if (x.at(i) == y.at(i) && i!=j && copyCode.at(i)!=2 && copyCode.at(i)!= 1) {
-				copyCode[i] = 2;
-				incorrect++;
-				break;
-			}
-		}
-	}
-	return incorrect;
-}
-*/
+        vector<int>::iterator it1, it2;	
 
+        vector<int> x1;
+	vector<int> y1;
+
+
+	// Transfer the unmatch ones to another vectors
 	for (int i = 0; i < x.size(); i++) {
 		if (x.at(i) != y.at(i)) {
 			x1.push_back(x.at(i));
 			y1.push_back(y.at(i));
-		}	
-	}
-
-	for (int i = 0; i < x1.size()-1; i++) {
-		if (x1.at(i) == x1.at(i+1)) {
-			x1.at(i) = -1;
 		}
 	}
 
@@ -97,17 +94,16 @@ int LockBox::incorrectLocation(LockBox *g) {
 			if ((x1.at(i) == y1.at(j)) && (i != j)) {
 				incorrect++;
 			}
-		}
+		} 
 	}
 	return incorrect;
 }
-/*
+
 int main() {
 
 	int length;
 	int digits;
 	int count = 0;
-	bool done = false;
 
 	cout << "Enter the lock code length: ";
 	cin >> length;
@@ -120,29 +116,24 @@ int main() {
 	string input;
 	LockBox *guess = new LockBox(length, digits);
 
-	while (!done && count++ < 10) {
+	bool done = false;
+
+	while (!done && count++ <= 10) {
 		cout << "Enter your guess separated by commas with no spaces: ";
 		cin >> input;
 		guess->setGuess(input);
-		
+
 		vector<int> code = answer->getCode();
 		vector<int> g = guess->getGuess();
 
 		if (code == g) {
-			cout << "Correct!" << endl;
+			cout << "correct! \n";
 			done = true;
-		}
-		else {
-			cout << "Numbers in correct location: " << answer->correctLocation(guess) << endl;
-			cout << "Numbers in incorrect location: " << answer->incorrectLocation(guess) << endl;
-		}
+		} else {
+			cout << answer->correctLocation(guess) << "," <<  answer->incorrectLocation(guess) << endl;
+        	}
+	}	
+
+	if (count > 10) cout << "system disabled \n"; 
+	return 0;
 }
-
-
-	if (count > 10) {
-		cout << "System disabled" << endl;
-	}
-
-return 0;
-}
-*/
